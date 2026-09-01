a76c1342c11f36ca18af8fd61a842856128dda3d
Anshuman-Sahu-23-06 <anshumansahu2306@gmail.com>
2026-09-01 20:08:21 +0530

    File Updated

--- src/routes/HealthCheck.routes.js
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import { healthCheck } from "../controllers/HealthCheck.controllers.js";
-
-const router = Router();
-
-router.route("/").get(healthCheck)
-router.route("/instagram").get(healthCheck)
-
-export default router;
