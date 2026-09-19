a1238bf7d6257d0c74a4d610b6d5771b7dabc38f
anshuman-sahu-dev <toanshumansahu@gmail.com>
2026-09-19 12:57:56 +0530

    File Updated

--- Backend/src/models/project.models.js
+++ Backend/src/models/project.models.js
@@ -4,7 +4,7 @@ const projectSchema = new Schema(
   {
     name: { type: String, required: true, unique: true, trim: true },
     description: { type: String },
-    CreatedBy: { type: Schema.Types.ObjectId, ref: "User", required: true },
+    createdBy: { type: Schema.Types.ObjectId, ref: "User", required: true },
   },
 
   { timestamps: true },
